f6be0cdee5bc1ae674e16e9179b8e1d9e6c717cd
amphoria <34939932+amphoria@users.noreply.github.com>
2024-03-12 17:48:38 +0000

    Add multi-file support to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
+import {resolve} from 'path'
 import {defineConfig} from 'vite'
 
 
 export default defineConfig({
-	plugins: []	
+	build: {
+		rollupOptions: {
+			input: {
+				index: resolve(__dirname, 'index.html'),
+				blog: resolve(__dirname, 'blog.html'),
+				about: resolve(__dirname, 'about.html'),
+			},
+		},
+	},
 })
